20d67ea3d7f6e997a3122043fac6444392814a74
Jhen D'Òliver Morious <jandol1996@hotmail.com>
2017-04-20 15:59:06 +0000

    EXAMEN EMPEZADO

---
+++ examenUF2-batallanaval/BatallaNaval.java
@@ -0,0 +1,29 @@
+public class BatallaNaval
+{
+    // APP CONSTANTS
+    int WIDTH = 10, HEIGHT = 10, BOATS = 5, MAXSIZE = 3;
+    char[] ORIENTATIONS = {'V','H'};
+    char[][] board = new char[WIDTH][HEIGHT];
+    
+    public static void main(String[] args)
+    {
+        inicializeBoard();
+        for(int i=0; i<BOATS; i++) // It's not my fault if java's random method is that messy
+            placeBoat((int)((Math.random()*(MAXSIZE-1))+1),ORIENTATIONS[(int)(Math.random()*(ORIENTATIONS.length-1))]);
+        
+    }
+    
+    public void inicializeBoard()
+    {
+        for(int i=0; i<WIDTH; i++)
+        {
+            for(int j=0; j<HEIGHT; j++)
+                board[i][j] = '~';
+        }
+    }
+    
+    public void placeBoat(int npos, char orientation)
+    {
+        
+    }
+}
